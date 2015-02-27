7d279ea01f4922f3bfa2c660614c67325bf9a1fa
Andrew dEX <andrewdexis@gmail.com>
2015-02-27 18:01:44 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WarmJS
+WarmJS is a library to guide developers to write code in best practices by providing practical examples in Pure javaScript.
+
+#Learn Pure/Vanilla JavaScript With Practical/Understandable Examples. 
+
+
+
+
--- js/warm.js
+++ js/warm.js
@@ -43,21 +43,3 @@
 
 }());
 
-
-/*Public Class Instance within the warm namespace*/
-var instance1 = new warm.exampleClass1();
-
-/*Access A method in the class*/
-alert(instance1.property1);
-
-/*Access A method in the class*/
-instance1.method();
-
-/*exampleClass2 Instance*/
-var instance2 = new warm.exampleClass2();
-
-/*Private Variables can not be accessed from the outside*/
-alert(instance2.privateProperty1); //Displays undefined since the inside variables are available to exampleClass2;
-
-/*Private Methods can not be accessed from outside*/
-instance2.privateMethod(); //
